5c335ba00f49440c00bc5770f9e93b55d0822c1f
SH:D_Sihyeon <admin@shd.one>
2022-11-10 15:29:02 +0900

    11.10 final

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,6 +7,8 @@ import Content from "../components/Content.js";
 import Appcontext from "../context/Appcontext.js";
 import { getPostOne } from "../common/common.function.js";
 import PostWrap from "../components/PostWrap.js";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 function Main() {
   const [selected, setSelected] = useState(null);
@@ -81,7 +83,7 @@ function Main() {
         </LeftContent>
       )}
       <RightWrap selected={selected}>
-        <RightHeader>
+        <RightHeader visible={openPost.length !== 0 ? true : false}>
           {openPost.map((one, index) => {
             const data = getPostOne(postData, one);
             return (
@@ -111,7 +113,10 @@ function Main() {
             );
           })}
         </RightHeader>
-        <RightContent selected={selected}>
+        <RightContent
+          selected={selected}
+          visible={openPost.length !== 0 ? true : false}
+        >
           {(() => {
             const data = getPostOne(postData, selectedPost);
             return (
@@ -122,11 +127,16 @@ function Main() {
                     <h1>{data?.title}</h1>
                     <p>Sihyeon | {data?.data?.date}</p>
                     <div>
-                      {data?.data?.tag.map((one, index) => (
+                      {data?.data?.tag?.map((one, index) => (
                         <span key={index}>{one}</span>
                       ))}
                     </div>
-                    <div>{data?.data?.content}</div>
+                    <div>
+                      <ReactMarkdown
+                        children={data.data?.content}
+                        remarkPlugins={[remarkGfm]}
+                      />
+                    </div>
                   </div>
                 </>
               )
@@ -209,7 +219,7 @@ const LeftContent = styled.div`
 `;
 const RightContent = styled.div`
   width: 100%;
-  height: calc(100% - 50px);
+  height: ${({ visible }) => (visible ? "calc(100% - 50px)" : "100%")};
   background-color: ${({ theme }) => theme.color.primary};
   padding: 10px 20px;
   display: flex;
@@ -243,7 +253,7 @@ const RightContent = styled.div`
 const RightHeader = styled.div`
   width: 100%;
   height: 50px;
-  display: flex;
+  display: ${({ visible }) => (visible ? "flex" : "none")};
   overflow-x: scroll;
   background-color: ${({ theme }) => theme.color.secondary};
   ::-webkit-scrollbar-thumb {
